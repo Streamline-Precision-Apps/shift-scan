42a33a02e8a0f64895402563c3f33ecdf47ae381
Streamline Apps <122407589+devunfox15@users.noreply.github.com>
2025-11-12 14:02:59 -0700

    added useEffect to push ios &  android user to signIn

--- client/app/admins/layout.tsx
+++ client/app/admins/layout.tsx
@@ -1,18 +1,26 @@
 "use client";
 import { Toaster } from "@/app/v1/components/ui/sonner";
 import LeftSidebar from "./_pages/sidebar/leftSide";
-import {
-  Sidebar,
-  SidebarProvider,
-  SidebarInset,
-} from "@/app/v1/components/ui/sidebar";
+import { Sidebar, SidebarProvider } from "@/app/v1/components/ui/sidebar";
 import { FcmProvider } from "./_pages/sidebar/FcmContext";
 import { DashboardDataProvider } from "./_pages/sidebar/DashboardDataContext";
+import { useRouter } from "next/router";
+import { Capacitor } from "@capacitor/core";
+import { useEffect } from "react";
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const router = useRouter();
+  const platform = Capacitor.getPlatform();
+
+  useEffect(() => {
+    if (platform === "ios" || platform === "android") {
+      router.push("/signin"); // Redirect to sign-in page
+    }
+  }, [platform, router]);
+
   return (
     <div className="flex h-screen w-full  bg-linear-to-b from-app-dark-blue via-app-blue to-app-blue ">
       <DashboardDataProvider>
